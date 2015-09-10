8c2f343a6967993d1184f538ad48f28379c387ea
Mike Perrone <mike.j.perrone@gmail.com>
2015-09-10 16:06:50 -0400

    refactor heuristic to be a little more understandable

--- solution/heuristic.py
+++ solution/heuristic.py
@@ -15,64 +15,46 @@ RIGHT = 4
 LEFT = 5
 NOTHING = 3
 
+def extract_coords(coord):
+    return coord[0], coord[1]
+
 
 def heuristic1(node_coords, node_dir, goal_coords):
     return 0
 
 
 def heuristic2(node_coords, node_dir, goal):
-    startrow = node_coords[0]
-    startcol = node_coords[1]
-    goalrow = goal[0]
-    goalcol = goal[1]
+    startrow, startcol = extract_coords(node_coords)
+    goalrow, goalcol = extract_coords(goal)
     difrow = abs(startrow - goalrow)
     difcol = abs(startcol - goalcol)
 
-    if difrow > difcol:
-        return difcol
-    else:
-        return difrow
+    return min(difrow, difcol)
 
 
 def heuristic3(node_coords, node_dir, goal):
-    start = node_coords
-    startrow = start[0]
-    startcol = start[1]
-    goalrow = goal[0]
-    goalcol = goal[1]
+    startrow, startcol = extract_coords(node_coords)
+    goalrow, goalcol = extract_coords(goal)
     difrow = abs(startrow - goalrow)
     difcol = abs(startcol - goalcol)
 
-    if difrow > difcol:
-        return difrow
-    else:
-        return difcol
+    return max(difrow, difcol)
 
 
 def heuristic4(node_coords, node_dir, goal):
-    start = node_coords
-    startrow = start[0]
-    startcol = start[1]
-    goalrow = goal[0]
-    goalcol = goal[1]
+    startrow, startcol = extract_coords(node_coords)
+    goalrow, goalcol = extract_coords(goal)
     difrow = abs(startrow - goalrow)
     difcol = abs(startcol - goalcol)
-    difsum = difrow + difcol
 
-    return difsum
+    return difrow + difcol
 
 
 def heuristic5(node_coords, node_dir, goal):
-    start = node_coords
-    startrow = start[0]
-    startcol = start[1]
-    goalrow = goal[0]
-    goalcol = goal[1]
-    difabsrow = abs(startrow - goalrow)
-    difabscol = abs(startcol - goalcol)
+    startrow, startcol = extract_coords(node_coords)
+    goalrow, goalcol = extract_coords(goal)
     difrow = startrow - goalrow
     difcol = startcol - goalcol
-    difsum = difabsrow + difabscol
     direction = UP
 
     if difrow == 0:
@@ -99,52 +81,13 @@ def heuristic5(node_coords, node_dir, goal):
 
     # Used to calculate the minimum amount of turning needed
 
-    turnsum = difsum + turn
+    turnsum = abs(difrow) + abs(difcol) + turn
 
     return turnsum
 
 
 def heuristic6(node_coords, node_dir, goal):
-    start = node_coords
-    startrow = start[0]
-    startcol = start[1]
-    goalrow = goal[0]
-    goalcol = goal[1]
-    difabsrow = abs(startrow - goalrow)
-    difabscol = abs(startcol - goalcol)
-    difrow = startrow - goalrow
-    difcol = startcol - goalcol
-    difsum = difabsrow + difabscol
-
-    direction = UP
-
-    if difrow == 0:
-        rowdirection = NOTHING
-    elif difrow > 0:
-        rowdirection = UP
-    else:
-        rowdirection = DOWN
-
-    if difcol == 0:
-        coldirection = NOTHING
-    elif difcol > 0:
-        coldirection = RIGHT
-    else:
-        coldirection = LEFT
-
-    if direction == rowdirection or direction == coldirection:
-        if rowdirection == NOTHING or coldirection == NOTHING:
-            turn = 0
-        else:
-            turn = .3
-    else:
-        turn = .6
-
-    # Used to calculate the minimum amount of turning needed
-
-    turnsum = difsum + turn
-
-    return 3*turnsum
+    return 3 * heuristic5(node_coords, node_dir, goal)
 
 H_MAP = {1: heuristic1,
          2: heuristic2,
